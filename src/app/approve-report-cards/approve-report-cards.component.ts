76937d975c575b7ad986342951e1e183045a3f1e
Jordyn Miller-Burko <jordyn@Jordyns-MacBook-Pro.local>
2019-03-02 23:24:30 -0500

    adjusting approve

--- src/app/approve-report-cards/approve-report-cards.component.ts
+++ src/app/approve-report-cards/approve-report-cards.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Level } from '../interfaces/level';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-approve-report-cards',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApproveReportCardsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private data: DataService) { }
+
+  levels: Level[];
 
   ngOnInit() {
+    this.data.getLevels().subscribe((data : Level[]) => {
+      console.log(data);
+
+      this.levels = data;
+      console.log(this.levels[0].name);
+    });
+
   }
 
 }
