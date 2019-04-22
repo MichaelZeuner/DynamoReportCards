bfa0b3bd232980be621b2df30ef4ade9af62a4b7
Michael Zeuner <michaelzeuner1996@gmail.com>
2019-04-21 23:20:43 -0400

    began adding modify to users

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { User } from '../interfaces/user';
 import { ErrorApi } from '../interfaces/error-api';
+import { MatInput, MatButtonToggle, MatButtonToggleGroup } from '@angular/material';
+import { DialogService } from '../shared/dialog.service';
 
 @Component({
   selector: 'app-users',
@@ -12,7 +14,7 @@ export class UsersComponent implements OnInit {
 
   public users: User[] = [];
 
-  constructor(private data: DataService) { }
+  constructor(private data: DataService, private dialog: DialogService) { }
 
   ngOnInit() {
     this.data.getUsers().subscribe(
@@ -21,6 +23,24 @@ export class UsersComponent implements OnInit {
         console.log(this.users);
       }
     );
+  }
+
+  updateUser(firstName: MatInput, 
+              lastName: MatInput, 
+              username: MatInput, 
+              email: MatInput, 
+              password: MatInput, 
+              passwordConfirm: MatInput,
+              accessLevel: MatButtonToggleGroup) {
+    if(password.value === passwordConfirm.value) {
+      console.log(firstName.value);
+      console.log(lastName.value);
+      console.log(username.value);
+      console.log(email.value);
+      console.log(accessLevel.value);
+    } else {
+      this.dialog.openSnackBar("Password and Confirm Password do not match.");
+    }
   }
 
 }
